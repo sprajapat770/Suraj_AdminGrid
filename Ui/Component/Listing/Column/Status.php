9761c76a0cbb5f72fb2b475624cfd7c4c50892fd
suraj <sprajapat770@gmail.com>
2021-01-11 16:56:24 +0530

    2nd commit

---
+++ Ui/Component/Listing/Column/Status.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Suraj\AdminGrid\Ui\Component\Listing\Column;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class Status implements ArrayInterface
+{
+
+    /**
+     * Return array of options as value-label pairs
+     *
+     * @return array Format: array(array('value' => '<value>', 'label' => '<label>'), ...)
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 1, 'label' => __('Enable')],
+            ['value' => 0, 'label' => __('Disable')]
+        ];
+    }
+}
